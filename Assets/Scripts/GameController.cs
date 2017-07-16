327bed14b250983c0615b585c698660717a7f57f
Wemko Dijkhuis <wemko.dijkhuis@trivento.nl>
2017-07-16 14:08:54 +0200

    initial project

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public enum GameState {Init, Ready, Play, GameOver};
+	private static GameState currentState = GameState.Init;
+
+	// Use this for initialization
+	void Awake () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (currentState)
+		{
+			//initialization from Game
+			case GameState.Init:
+				break;
+			//Game is ready to play
+			case GameState.Ready:
+				break;
+			//Game is playing
+			case GameState.Play:
+				break;
+			//Game is over
+			case GameState.GameOver:
+				break;
+			default:
+				break;
+		}
+	}
+}
