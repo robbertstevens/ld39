66961e17d006a5cc92f7224f8aed26b058488f94
Robbert Stevens <Robbert Stevens>
2017-07-29 13:26:24 +0200

    Added new sprites, added generator

---
+++ Assets/Scripts/Generator/AddEnergyToPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddEnergyToPlayer : MonoBehaviour
+{
+    private int energy = 0;
+
+    public int FuelRatio = 10;
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("Healing Player");
+        if (collision.gameObject.tag != Tag.Player)
+        {
+            return;
+        }
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+        player.Energy += HealPlayer();
+    }
+
+    private int HealPlayer()
+    {
+        return energy--;
+    }
+
+    public void AddFuel(int fuel)
+    {
+        energy += fuel * FuelRatio;
+    }
+}
