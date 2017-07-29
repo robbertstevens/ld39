e61705078567df99e5b0461435289a3f4167371d
Wemko Dijkhuis <wemko.dijkhuis@trivento.nl>
2017-07-29 14:13:21 +0200

    rotating player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour {
 	public float FireRate = 1.0f;
 	public enum PlayerState { Alive, Dead }
 	public PlayerState State = PlayerState.Alive;
+	public GameObject PlayerSprite;
 
 	// Use this for initialization
 	void Start () {
@@ -20,6 +21,7 @@ public class PlayerController : MonoBehaviour {
 		{
 			case PlayerState.Alive:
 				MovePlayer();
+				RotateSprite();
 				Shoot();
 			break;
 			case PlayerState.Dead:
@@ -29,14 +31,44 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 
-	public void MovePlayer(){
+	private void MovePlayer(){
 		transform.Translate(new Vector2(0,1) * Speed * Input.GetAxis("Vertical") * Time.deltaTime);
 		transform.Translate(new Vector2(1,0) * Speed * Input.GetAxis("Horizontal") * Time.deltaTime);
 	}
 
-	public void Shoot() {
-		if(Input.GetButton("Fire")){
+	private void Shoot() {
+		if(Input.GetButton("Fire1")){
 			// DO SHOOTING
 		}
+	}
+
+	private void RotateSprite() {
+		float rotationDegree = GetRotation();
+		PlayerSprite.transform.rotation = Quaternion.Euler(0,0,rotationDegree);
+	}
+
+	private float GetRotation(){
+		if(Input.GetAxis("Vertical") > 0){
+			if(Input.GetAxis("Horizontal") > 0){
+				return 315.0f;
+			}else if(Input.GetAxis("Horizontal") < 0) {
+				return 45.0f;
+			}
+			return 0;
+		}else if(Input.GetAxis("Vertical") < 0){
+			if(Input.GetAxis("Horizontal") > 0){
+				return 225.0f;
+			}else if(Input.GetAxis("Horizontal") < 0) {
+				return 135.0f;
+			}
+			return 180.0f;
+		}else if(Input.GetAxis("Horizontal") != 0){
+			if(Input.GetAxis("Horizontal") > 0){
+				return 270.0f;
+			}else if(Input.GetAxis("Horizontal") < 0) {
+				return 90.0f;
+			}
+		}
+		return PlayerSprite.transform.rotation.eulerAngles.z;
 	}
 }
