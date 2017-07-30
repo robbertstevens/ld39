e56dbc1eaaa531e338ea7e7847dcd885704af2ea
Wemko Dijkhuis <wemko.dijkhuis@trivento.nl>
2017-07-30 15:29:14 +0200

    GameOverScreen

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverScript : MonoBehaviour {
+
+	public GameObject Visible;
+	public PlayerController PlayerScript;
+
+	// Update is called once per frame
+	void Update () {
+		if(!Visible.activeSelf && PlayerScript.State == PlayerController.PlayerState.Dead){
+			Visible.SetActive(true);
+		}
+
+		if(Visible.activeSelf && Input.anyKey){
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+}
